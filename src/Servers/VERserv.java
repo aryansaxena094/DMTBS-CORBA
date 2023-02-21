b43a2e22c954942882c19290260cd5af83ff245d
Aryan <aryan.saxena094@gmail.com>
2023-02-21 16:21:40 -0500

    commit

--- src/Servers/VERserv.java
+++ src/Servers/VERserv.java
@@ -1,5 +1,48 @@
 package Servers;
 
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContext;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextExtOperations;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import ServerSide.Server;
+import ServerSide.ServerHelper;
+
 public class VERserv {
+    public static String servername = "Verdun";
+
+    public static void main(String[] args) {
+       try {
+        ORB orb = ORB.init(args, null);
+
+        POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+
+        rootpoa.the_POAManager().activate();
+
+        ATWImpl serv1 = new ATWImpl();
+        serv1.setOrb(orb);
+        
+        org.omg.CORBA.Object ref = rootpoa.servant_to_reference(serv1);
+        Server href = ServerHelper.narrow(ref);
+
+        org.omg.CORBA.Object objref = orb.resolve_initial_references("NameService");
+
+        NamingContext ncRef = NamingContextExtHelper.narrow(objref);
+
+        NameComponent path[] = ((NamingContextExtOperations) ncRef).to_name("VER");
+        
+        ncRef.rebind(path, href);
+
+        System.out.println(servername + " server is up and running!");
+
+        orb.run();
+
+       } catch (Exception e) {
+        e.printStackTrace();
+       } 
+    }
     
 }
